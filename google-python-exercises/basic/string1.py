fa919960925908b572334dd899207cc6cd2e7c39
= <pjfritch1@gmail.com>
2017-11-23 10:45:47 -0800

    string exercises

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -56,7 +56,7 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
+  
   return
 
 
