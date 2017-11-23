f092c2a7d9dd5460f34337d8d06371ee8c0ad14e
= <pjfritch1@gmail.com>
2017-11-23 11:06:18 -0800

    string exercises

--- google-python-exercises/basic/string2.py
+++ google-python-exercises/basic/string2.py
@@ -56,7 +56,8 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  # +++your code here+++
+  a_len = len(a)
+  b_len = len(b)
   return
 
 
